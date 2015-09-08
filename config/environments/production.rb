c43ebc1306b967b7b2b860096cc96161a2333819
BuddhistWalrus <Gabe>
2015-09-08 15:33:58 -0400

    weee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,14 +17,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'greenstogrounds.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "login",
-    enable_starttls_auto: true,
+    address: "smtp.mandrillapp.com",
+    port: 25,
     user_name: "gabecorso@gmail.com",
-    password: "ceciliacorso"
+    password: "aIN4AKT8xEw5uwqHBE01ow"
   }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
