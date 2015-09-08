738d5b730cce096e3a5c63a919174ad65bd1c480
BuddhistWalrus <Gabe>
2015-09-08 15:16:17 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,11 +16,11 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost3000' }
+  config.action_mailer.default_url_options = { host: 'greenstogrounds.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "localhost:3000",
+    domain: "greenstogrounds.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "gabecorso@gmail.com",
